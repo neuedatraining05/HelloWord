87e3f68c8c5bbe91520aa4ee0cd454584153b504
gloire lutete <luteteg@yahoo.ca>
2022-07-26 15:33:08 +0000

    1st commin

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+//psvm
+    public static void main(String[] args) {
+        //sout
+        int g = 8;
+        System.out.println("hello world" + g);
+
+        for(int x=50;x<56;x++){
+            System.out.println(x*3);
+        }
+        int arr[] ={1,2,3,4,5,6,7,8,9,10};
+        int counter=0;
+        while(arr[counter]<7){
+            System.out.println(arr[counter]);
+            counter ++;
+        }
+        int test=0;
+        switch (test){
+            case 0:
+                System.out.println("dd");
+                break;
+            case 2:
+                System.out.println("dd");
+                break;
+            default:
+                System.out.println("ddd");
+                break;
+        }
+
+    }
+}
